5369bd00fb5676ddc09d1a81efd9d097c609e769
FG98 <81941690+FG98F@users.noreply.github.com>
2021-09-21 19:23:36 -0400

    Add files via upload

--- fg.js
+++ fg.js
@@ -37,6 +37,7 @@ const { spawn, exec, execSync } = require("child_process");
 const fs = require("fs");
 const axios = require("axios");
 const ffmpeg = require("fluent-ffmpeg");
+const imgbb = require('imgbb-uploader');
 const { EmojiAPI } = require("emoji-api");
 const ig = require("insta-fetcher");
 const emoji = new EmojiAPI();
@@ -539,48 +540,7 @@ const linkwa = 'https://chat.whatsapp.com'
 		}, 0)
 	}
 	
-	//-------------𝗦𝗧𝗜𝗖𝗞𝗘𝗥 𝗠𝗔𝗞𝗘𝗥 ---------
-			function addMetadata(packname, author) {	
-				if (!packname) packname = 'DyLux-FG'; if (!author) author = pushname ;	
-				author = author.replace(/[^a-zA-Z0-9]/g, '');	
-				let name = `${author}_${packname}`
-				if (fs.existsSync(`./sticker/${name}.exif`)) return `./sticker/${name}.exif`
-				const json = {	
-					"sticker-pack-name": packname,
-					"sticker-pack-publisher": author,
-				}
-				const littleEndian = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00])	
-				const bytes = [0x00, 0x00, 0x16, 0x00, 0x00, 0x00]	
-
-				let len = JSON.stringify(json).length	
-				let last	
-
-				if (len > 256) {	
-					len = len - 256	
-					bytes.unshift(0x01)	
-				} else {	
-					bytes.unshift(0x00)	
-				}	
-
-				if (len < 16) {	
-					last = len.toString(16)	
-					last = "0" + len	
-				} else {	
-					last = len.toString(16)	
-				}	
-
-				const buf2 = Buffer.from(last, "hex")	
-				const buf3 = Buffer.from(bytes)	
-				const buf4 = Buffer.from(JSON.stringify(json))	
-
-				const buffer = Buffer.concat([littleEndian, buf2, buf3, buf4])	
-
-				fs.writeFile(`./sticker/${name}.exif`, buffer, (err) => {	
-					return `./sticker/${name}.exif`	
-				})	
-
-			}
-
+	
 //---colores 
     colors = ["red", "white", "black", "blue", "yellow", "green"];
    
