71446fba9fdec51c83ace96b13c7f2ae2d03f8a8
ketzakot <77943259+ketzakot@users.noreply.github.com>
2021-09-23 22:13:54 -0500

    Update fg.js

--- fg.js
+++ fg.js
@@ -588,6 +588,10 @@ const sendMessageFromContent = async(jid, message, options) => {
 buf = fs.readFileSync(`./src/mp3/holabot.mp3`)
 Fg.sendMessage(from, buf, audio, {mimetype: 'audio/mp4', quoted: mek, ptt: true})
 break
+	case 'hola bot':
+buf = fs.readFileSync(`./src/mp3/holabot.mp3`)
+Fg.sendMessage(from, buf, audio, {mimetype: 'audio/mp4', quoted: mek, ptt: true})
+break
         	
  case 'prefix':
  case 'prefijo': 
