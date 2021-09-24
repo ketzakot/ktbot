b0503b2f9ba5bab48aca9693ed3a7f2c84174c0e
ketzakot <77943259+ketzakot@users.noreply.github.com>
2021-09-23 21:36:03 -0500

    Update fg.js

--- fg.js
+++ fg.js
@@ -588,12 +588,6 @@ const sendMessageFromContent = async(jid, message, options) => {
 buf = fs.readFileSync(`./src/mp3/holabot.mp3`)
 Fg.sendMessage(from, buf, audio, {mimetype: 'audio/mp4', quoted: mek, ptt: true})
 break
-        switch(autofg) {
-        	
-        case 'puto bot':
-buf = fs.readFileSync(`./src/mp3/putobot.mp3`)
-Fg.sendMessage(from, buf, audio, {mimetype: 'audio/mp4', quoted: mek, ptt: true})
-break
         	
  case 'prefix':
  case 'prefijo': 
